c12616f7e49af52f01e35777947145e33f95decf
Lux <9366973+Luxulicious@users.noreply.github.com>
2020-02-05 14:09:21 +0100

    Added some fairly standard methods to int variable

--- Core/Runtime/Variables/Primitives/IntVariable.cs
+++ Core/Runtime/Variables/Primitives/IntVariable.cs
@@ -6,5 +6,14 @@ namespace SOA.Common.Primitives
     [CreateAssetMenu(fileName = "New Int Variable", menuName = "SOA/Primitives/Int/Variable", order = 1)]
     public class IntVariable : Variable<int, IntUnityEvent, IntIntUnityEvent>
     {
+        public void Decrement(int i)
+        {
+            Value -= i;
+        }
+
+        public void Increment(int i)
+        {
+            Value += i;
+        }
     }
 }
