0f04fe64d15f6b7489c255c18a47a43892a8894c
Lux <9366973+Luxulicious@users.noreply.github.com>
2020-02-10 15:23:11 +0100

    Variable editor now displays components and game objects in their respective fields
    This can hopefully replace (static) singletons where neccesary

--- Core/Editor/Variables/Base/VariableEditor.cs
+++ Core/Editor/Variables/Base/VariableEditor.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+﻿using System.Xml.Schema;
+using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.Events;
@@ -28,19 +29,53 @@ namespace SOA.Base
 
             if (!isPlaying)
                 EditorGUILayout.PropertyField(useAsConstantProperty, true);
-
+            
             if (useAsConstantProperty.boolValue)
+            {
                 EditorGUILayout.PropertyField(!isPlaying ? defaultValueProperty : runtimeValueProperty,
                     new GUIContent("Constant Value"), true);
+            }
             else
             {
                 EditorGUI.BeginDisabledGroup(isPlaying);
                 EditorGUILayout.PropertyField(defaultValueProperty, new GUIContent("Default Value"), true);
                 EditorGUI.EndDisabledGroup();
-                EditorGUI.BeginDisabledGroup(!isPlaying);
-                EditorGUILayout.PropertyField(runtimeValueProperty, new GUIContent("Runtime Value"), true);
-                EditorGUI.EndDisabledGroup();
-                
+                if (runtimeValueProperty.IsPartOfAnyScene())
+                {
+                    Debug.Log("Scene object detected!");
+                    if (!isPlaying)
+                    {
+                        runtimeValueProperty.SetValue(null);
+                        EditorGUI.BeginDisabledGroup(true);
+                        EditorGUILayout.PropertyField(runtimeValueProperty, new GUIContent("Runtime Value"), true);
+                        EditorGUI.EndDisabledGroup();
+                    }
+                    else
+                    {
+                        EditorGUI.BeginDisabledGroup(true);
+                        EditorGUILayout.TextField(runtimeValueProperty?.displayName, runtimeValueProperty.GetValue()?.ToString());
+                        EditorGUI.EndDisabledGroup();
+                        if (GUILayout.Button("Go to reference"))
+                        {
+                            var instance = (runtimeValueProperty.GetValue() as GameObject);
+                            if (!instance) instance = (runtimeValueProperty.GetValue() as Component)?.gameObject;
+                            var instanceId = instance.GetInstanceID();
+                            Selection.activeInstanceID = instanceId;
+                            Selection.activeGameObject = instance;
+                        }
+                    }
+                }
+                else
+                {
+                    EditorGUI.BeginDisabledGroup(!isPlaying);
+                    EditorGUILayout.PropertyField(runtimeValueProperty, new GUIContent("Runtime Value"), true);
+                    EditorGUI.EndDisabledGroup();
+                }
+
+                var isRunTimeOnlyType = typeof(T) == typeof(GameObject) || typeof(T) == typeof(Component);
+                if (isRunTimeOnlyType && !isPlaying) EditorGUILayout.LabelField($"({typeof(T).Name} Variables can only be assigned to at runtime without inspector)");
+
+
                 EditorGUILayout.PropertyField(serializedObject.FindProperty("_onChangeEvent"), true);
                 EditorGUILayout.PropertyField(serializedObject.FindProperty("_onChangeWithHistoryEvent"), true);
 
