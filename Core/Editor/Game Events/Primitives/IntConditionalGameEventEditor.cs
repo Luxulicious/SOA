6a6f0107a0462a1ade5864c1856463e4543a2372
Lux <9366973+Luxulicious@users.noreply.github.com>
2020-02-20 13:13:49 +0100

    Moving the new conditionals to the package

---
+++ Core/Editor/Game Events/Primitives/IntConditionalGameEventEditor.cs
@@ -0,0 +1,11 @@
+﻿using SOA.Common.Primitives;
+using UnityEditor;
+
+[CustomEditor(typeof(IntConditionalGameEvent))]
+public class IntConditionalGameEventEditor : IntGameEventEditor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+    }
+}
---
+++ Core/Runtime/Game Events/Base/ConditionalGameEvent.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace SOA.Base
+{
+    [Serializable]
+    public enum ComparisonOperator
+    {
+        Equals,
+        LessThan,
+        GreaterThan,
+        Unequal
+    }
+
+    [Serializable]
+    public class Condition<T>
+    {
+        [SerializeField] protected ComparisonOperator _operator;
+
+        [SerializeField] protected T _valueToCompare;
+
+        public virtual bool Compare(T value)
+        {
+            var comparison = Comparer<T>.Default.Compare(value, _valueToCompare);
+            switch (_operator)
+            {
+                case ComparisonOperator.Equals:
+                    return comparison == 0;
+                case ComparisonOperator.LessThan:
+                    return comparison > 0;
+                case ComparisonOperator.GreaterThan:
+                    return comparison < 0;
+                case ComparisonOperator.Unequal:
+                    return comparison != 0;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+
+    //TODO This class can't currently be implemented properly since <T>GameEventListeners won't pick this as a valid ScriptableObject
+    [Obsolete(
+        "TODO This class can't currently be implemented properly since <T>GameEventListeners won't pick this as a valid ScriptableObject")]
+    public abstract class ConditionalGameEvent<C, E, T> : GameEvent<E, T>
+        where C : Condition<T> where E : UnityEvent<T>, new()
+    {
+        [SerializeField] protected C[] _conditions;
+
+        /// <summary>
+        /// Iterates over conditions and applies AND between them
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        protected virtual bool Compare(T value)
+        {
+            foreach (var c in _conditions)
+            {
+                if (!c.Compare(value))
+                    return false;
+            }
+
+            return true;
+        }
+
+        public override void Invoke(T value)
+        {
+            if (Compare(value))
+                base.Invoke(value);
+        }
+    }
+}
