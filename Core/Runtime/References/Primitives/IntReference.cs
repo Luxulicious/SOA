942a7543e3db122d38d47f5163921ee6f4c20094
Lux <9366973+Luxulicious@users.noreply.github.com>
2020-03-05 17:25:13 +0100

    Standard methods and constructor added for int reference

--- Core/Runtime/References/Primitives/IntReference.cs
+++ Core/Runtime/References/Primitives/IntReference.cs
@@ -6,5 +6,18 @@ namespace SOA.Common.Primitives
     [Serializable]
     public class IntReference : Reference<IntVariable, int, IntUnityEvent, IntIntUnityEvent>
     {
+        public IntReference(int value) : base(value)
+        {
+        }
+
+        public void Decrement(int i)
+        {
+            Value -= i;
+        }
+
+        public void Increment(int i)
+        {
+            Value += i;
+        }
     }
 }
