38796a66b7a9f22e5216ff2f017152af4f10dae2
Lux <9366973+Luxulicious@users.noreply.github.com>
2020-03-21 16:09:31 +0100

    Created a generic interface for inputs

--- Core/Runtime/References/UnityEngine/KeyCodeReference.cs
+++ Core/Runtime/References/UnityEngine/KeyCodeReference.cs
@@ -2,10 +2,24 @@ using System;
 using SOA.Base;
 using UnityEngine;
 
+//TODO Move to seperate file
+public interface IInput
+{
+    bool Active { get; }
+}
+
+//TODO Move to seperate file
+public interface IDigitalInput : IInput
+{
+}
+
 namespace SOA.Common.UnityEngine
 {
     [Serializable]
-    public class KeyCodeReference : Reference<KeyCodeVariable, KeyCode, KeyCodeUnityEvent, KeyCodeKeyCodeUnityEvent>
+    public class KeyCodeReference : Reference<KeyCodeVariable, KeyCode, KeyCodeUnityEvent, KeyCodeKeyCodeUnityEvent>,
+        IDigitalInput
+
     {
+        public bool Active => Input.GetKey(Value);
     }
 }
