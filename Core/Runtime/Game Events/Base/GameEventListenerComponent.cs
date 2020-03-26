0ba3fe43db154518b608c6d2447edcccd0c67223
Lux <9366973+Luxulicious@users.noreply.github.com>
2020-03-26 13:32:48 +0100

    Adding game event listener components

---
+++ Core/Runtime/Game Events/Base/GameEventListenerComponent.cs
@@ -0,0 +1,18 @@
+﻿using SOA.Base;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace SOA.Common.Primitives
+{
+    public class GameEventListenerComponent : MonoBehaviour
+    {
+        [SerializeField] private GameEventListener _listener;
+    }
+
+    public class GameEventListenerComponent<GEL, GE, E, T> : MonoBehaviour where GEL : GameEventListener<GE, E, T>
+        where GE : GameEvent<E, T>, new()
+        where E : UnityEvent<T>, new()
+    {
+        [SerializeField] protected GEL _listener;
+    }
+}
---
+++ Core/Runtime/Game Events/Primitives/FloatGameEventListenerComponent.cs
@@ -0,0 +1,11 @@
+﻿using SOA.Common.Primitives;
+
+namespace SOA.Common.Primitives
+{
+    public class
+        FloatGameEventListenerComponent : GameEventListenerComponent<FloatGameEventListener, FloatGameEvent,
+            FloatUnityEvent
+            , float>
+    {
+    }
+}
