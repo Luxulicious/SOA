5c2f7ade08a358ba949d5fb1d4d825d148af6232
Lux <9366973+Luxulicious@users.noreply.github.com>
2020-03-26 13:32:48 +0100

    Adding game event listener components

---
+++ Package/Core/Runtime/Game Events/Primitives/IntGameEventListenerComponent.cs
@@ -0,0 +1,11 @@
+﻿using SOA.Common.Primitives;
+using UnityEngine;
+
+namespace SOA.Common.Primitives
+{
+    public class
+        IntGameEventListenerComponent : GameEventListenerComponent<IntGameEventListener, IntGameEvent, IntUnityEvent,
+            int>
+    {
+    }
+}
