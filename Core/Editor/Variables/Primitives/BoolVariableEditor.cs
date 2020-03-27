a546480e51eb1bcfc301465be852b7a5c97aff79
Lux <9366973+Luxulicious@users.noreply.github.com>
2020-03-27 01:22:28 +0100

    BoolVariableEditor updated to contain the new composite bool changes

--- Core/Editor/Variables/Primitives/BoolVariableEditor.cs
+++ Core/Editor/Variables/Primitives/BoolVariableEditor.cs
@@ -1,11 +1,78 @@
 ﻿using SOA.Base;
 using UnityEditor;
+using UnityEngine;
 
 namespace SOA.Common.Primitives
 {
-    //[CustomEditor(typeof(BoolVariable))]
+    [CustomEditor(typeof(BoolVariable))]
     public class BoolVariableEditor : VariableEditor<BoolVariable, bool, BoolUnityEvent, BoolBoolUnityEvent>
     {
-       
+        //TODO Dynamically determine these string via member information of the class
+        protected static readonly string _compositePropertyPath = "_composite";
+        protected static readonly string _onValueChangedToTrueEventPropertyPath = "_onValueChangedToTrueEvent";
+        protected static readonly string _onValueChangedToFalseEventPropertyPath = "_onValueChangedToFalseEvent";
+        protected static readonly string _andOrPropertyPath = "_andOr";
+        protected static readonly string _memberValuesPropertyPath = "_memberValues";
+        
+        protected override void DrawPersistencePropertyField(SerializedProperty useAsConstantProperty)
+        {
+            var compositeProperty = serializedObject.FindProperty(_compositePropertyPath);
+            var compositeValue = compositeProperty.boolValue;
+            if (!compositeValue)
+                base.DrawPersistencePropertyField(useAsConstantProperty);
+        }
+
+        protected override void DrawDefaultValuePropertyField(SerializedProperty defaultValueProperty,
+            string label = null)
+        {
+            var compositeProperty = serializedObject.FindProperty(_compositePropertyPath);
+            var compositeValue = compositeProperty.boolValue;
+            if (!compositeValue)
+                base.DrawDefaultValuePropertyField(defaultValueProperty, label);
+        }
+
+        protected override void DrawInvokeOnChangeEventsButtons(BoolVariable variable)
+        {
+            base.DrawInvokeOnChangeEventsButtons(variable);
+            EditorGUI.BeginDisabledGroup(!Application.isPlaying);
+            if (GUILayout.Button("Invoke On Changed To True Event")) variable.InvokeOnChangedToTrueEvent();
+            if(GUILayout.Button("Invoke On Changed To False Event")) variable.InvokeOnChangedToFalseEvent();
+            EditorGUI.EndDisabledGroup();
+        }
+
+        protected override void DrawOnChangeEventsPropertyFields(SerializedProperty onChangedEventProperty,
+            SerializedProperty onChangedWithHistoryEventProperty)
+        {
+            base.DrawOnChangeEventsPropertyFields(onChangedEventProperty, onChangedWithHistoryEventProperty);
+            var onValueChangedToTrueEventProperty = serializedObject.FindProperty(_onValueChangedToTrueEventPropertyPath);
+            EditorGUILayout.PropertyField(onValueChangedToTrueEventProperty, true);
+            var onValueChangedToFalseEventProperty = serializedObject.FindProperty(_onValueChangedToFalseEventPropertyPath);
+            EditorGUILayout.PropertyField(onValueChangedToFalseEventProperty, true);
+        }
+
+        protected override void PostOnInspectorGUI()
+        {
+            base.PostOnInspectorGUI();
+            var compositeProperty = serializedObject.FindProperty(_compositePropertyPath);
+            EditorGUILayout.PropertyField(compositeProperty, true);
+            var compositeValue = compositeProperty.boolValue;
+            if (compositeValue)
+            {
+                var andOrProperty = serializedObject.FindProperty(_andOrPropertyPath);
+                EditorGUILayout.PropertyField(andOrProperty, true);
+                var memberValuesProperty = serializedObject.FindProperty(_memberValuesPropertyPath);
+                EditorGUILayout.PropertyField(memberValuesProperty, true);
+            }
+        }
+
+        protected override void DrawRuntimeValuePropertyField(SerializedProperty runtimeValueProperty, string label = null)
+        {
+            var compositeProperty = serializedObject.FindProperty(_compositePropertyPath);
+            var compositeValue = compositeProperty.boolValue;
+            if (!compositeValue)
+                base.DrawRuntimeValuePropertyField(runtimeValueProperty, label);
+            else
+                base.DrawRuntimeValuePropertyField(runtimeValueProperty, "Composite Value");
+        }
     }
 }
