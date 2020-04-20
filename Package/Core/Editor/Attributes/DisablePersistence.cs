2fad370304a2f3deeb898c724d6aca174d370f29
Lux <9366973+Luxulicious@users.noreply.github.com>
2020-04-20 22:07:28 +0200

    Added custom attribute DisablePersistenceAttribute and HideOnChangeEventsAttribute

---
+++ Package/Core/Editor/Attributes/DisablePersistence.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace SOA.Base
+{
+    /// <summary>
+    /// Disables persistence in the inspector
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field)]
+    public class DisablePersistence : PropertyAttribute
+    {
+        public DisablePersistence()
+        {
+        }
+    }
+}
