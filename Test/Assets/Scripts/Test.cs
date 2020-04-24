7f90a45c27333190065c5b1c9d0623058ab83d2f
Lux <9366973+Luxulicious@users.noreply.github.com>
2020-04-24 14:23:30 +0200

    Update test files

---
+++ Test/Assets/Scripts/Test.cs
@@ -0,0 +1,14 @@
+﻿using SOA.Base;
+using SOA.Common.Primitives;
+
+public class Test : RegisteredMonoBehaviour
+{
+    [DisablePersistence] public FloatReference someFloat = new FloatReference();
+
+    public override void Register()
+    {
+        someFloat.Register(this);
+    }
+
+
+}
